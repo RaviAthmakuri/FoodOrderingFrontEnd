8d2733da915d735ee3ee73ec6b459447c54e6432
ravi.athmakuri <ravi.athmakuri93@gmail.com>
2020-11-21 18:29:14 +0530

    Added Header and changed index to point it to Header

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import Controller from './screens/Controller';
+// import Controller from './screens/Controller';
+import Header from './common/Header';
 
-ReactDOM.render(<Controller/>, document.getElementById('root'));
+ReactDOM.render(<Header/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
